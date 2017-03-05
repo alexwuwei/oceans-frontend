90c67d900f8eb4dc666504546718198b9e960423
Hugo Guerra <guerrahugoa@gmail.com>
2017-03-04 21:46:23 -0800

    initializes needed files, dir structure and react sass via webpack

---
+++ dev/js/index.jsx
@@ -0,0 +1,29 @@
+'use strict';
+require(__dirname + '/../sass/home/base-home.scss');
+require(__dirname + '/../sass/home/module-home.scss');
+require(__dirname + '/../sass/home/layout-home.scss');
+require(__dirname + '/../sass/home/state-home.scss');
+
+import {Router, Route, hashHistory} from 'react-router';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+var Header = require('./header.jsx');
+var Ocean = require('./ocean.jsx');
+
+
+class App extends React.Component {
+    render() {
+        return <Header/>
+    }
+}
+//
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={Ocean}/>
+        <Route path="/about" component={Ocean}/>
+        <Route path="/landing" component={App}/>
+
+    </Router>
+), document.getElementById('app'));
